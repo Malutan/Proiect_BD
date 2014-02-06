97c1404f14ba4c8fbf1b7f57c5f56277329ab317
Malutan Sergiu <malutan_sergiu@yahoo.com>
2014-02-06 14:51:26 -0800

    nimic nou
    gggg

--- Baze_de_Date/Form1.Designer.cs
+++ Baze_de_Date/Form1.Designer.cs
@@ -29,64 +29,14 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.stare = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.btn_post = new System.Windows.Forms.Button();
-            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.btn_post);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.stare);
-            this.panel1.Controls.Add(this.textBox1);
             this.panel1.Location = new System.Drawing.Point(119, 1);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(574, 409);
             this.panel1.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(145, 19);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(171, 16);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Adauga poze/videoclipuri";
-            // 
-            // stare
-            // 
-            this.stare.AutoSize = true;
-            this.stare.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.stare.Location = new System.Drawing.Point(21, 19);
-            this.stare.Name = "stare";
-            this.stare.Size = new System.Drawing.Size(115, 16);
-            this.stare.TabIndex = 0;
-            this.stare.Text = "Actualizare stare";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(148, 52);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(225, 20);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.TabStop = false;
-            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
-            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
-            // 
-            // btn_post
-            // 
-            this.btn_post.Location = new System.Drawing.Point(298, 78);
-            this.btn_post.Name = "btn_post";
-            this.btn_post.Size = new System.Drawing.Size(75, 23);
-            this.btn_post.TabIndex = 2;
-            this.btn_post.Text = "Posteaza";
-            this.btn_post.UseVisualStyleBackColor = true;
-            this.btn_post.Click += new System.EventHandler(this.btn_post_Click);
             // 
             // Form1
             // 
@@ -96,8 +46,6 @@
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -105,10 +53,6 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label stare;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button btn_post;
 
     }
 }
--- Baze_de_Date/Form1.cs
+++ Baze_de_Date/Form1.cs
@@ -18,42 +18,44 @@ namespace Baze_de_Date
         {
             InitializeComponent();
             post p = new post(panel1);
-            post p1 = new post(panel1);
-            init_perete();
+        //    post p1 = new post(panel1);
+            post1 a = new post1(panel1);
+            post b = new post(panel1);
+            //init_perete();
         }
 
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-            if (textBox1.Text.Length > 20)
-            {
-                textBox1.Height += 20;
-                btn_post.Top+= 20;
-            }
-        }
+        //private void textBox1_TextChanged(object sender, EventArgs e)
+        //{
+        //    if (textBox1.Text.Length > 20)
+        //    {
+        //        textBox1.Height += 20;
+        //        btn_post.Top+= 20;
+        //    }
+        //}
 
 
-        public void init_perete()
-        {
-            textBox1.Text = "scrie ceva";
-            btn_post.Hide();
-        }
+        //public void init_perete()
+        //{
+        //    textBox1.Text = "scrie ceva";
+        //    btn_post.Hide();
+        //}
 
-        private void textBox1_Enter(object sender, EventArgs e)
-        {
-            if(textBox1.Text=="scrie ceva")
-              textBox1.Text = "";
-            btn_post.Visible = true;
-        }
+        //private void textBox1_Enter(object sender, EventArgs e)
+        //{
+        //    if(textBox1.Text=="scrie ceva")
+        //      textBox1.Text = "";
+        //    btn_post.Visible = true;
+        //}
 
-        private void textBox1_Leave(object sender, EventArgs e)
-        {
-            string txt = textBox1.Text;
-            if (txt == "") 
-                textBox1.Text="scrie ceva";
-            btn_post.Hide();
+        //private void textBox1_Leave(object sender, EventArgs e)
+        //{
+        //    string txt = textBox1.Text;
+        //    if (txt == "") 
+        //        textBox1.Text="scrie ceva";
+        //    btn_post.Hide();
 
-        }
+        //}
 
         private void btn_post_Click(object sender, EventArgs e)
         {
@@ -61,16 +63,63 @@ namespace Baze_de_Date
         //    scriu statusul in baza de date
         }
 
+    };
+    
+    public class antet
+    {
+        PictureBox coperta;
+        PictureBox profil;
+       public antet(){
+    //       coperta;
+           
+       }
     }
+
+    public class post1{
+        TextBox  txt;
+        Label lbl;
+        int top = 150;
+        int left = 200;
+        Button btn;
+        public post1(Panel p)
+        {
+            btn = new Button();
+            btn.Name = "posteaza";
+            btn.Text = "posteaza";
+            btn.Top = top + 20;
+            btn.Left = left + 200;
+            p.Controls.Add(btn);
+            lbl = new Label();
+            lbl.Name = "lbl2";
+            lbl.Text = "Scrie pe perete";
+            lbl.Top = top - 25;
+            lbl.Left = left;
+            lbl.Font = new System.Drawing.Font("Arial", 9, FontStyle.Bold);
+            lbl.ForeColor = System.Drawing.Color.Blue;
+            p.Controls.Add(lbl);
+            txt = new TextBox();
+            txt.Name = "textBox2";
+            txt.Text = "Scrie ceva";
+            txt.Top = top;
+            txt.Width = 300;
+            txt.Left = left;
+            txt.TabStop = false;
+            p.Controls.Add(txt);
+        }
+
+    };
 
     public class post
     {
         TextBox txt;
-        static int top = 110;
-        int left = 60;
+        Label lbl;
+        Label com;
+        Label like;
+        static int top = 260;
+        int left = 200;
         public post(Panel p)
         {
-            Label lbl = new Label();
+             lbl = new Label();
             lbl.Name = "lbl2";
             lbl.Text = "A scris";
             lbl.Top = top - 25;
@@ -85,7 +134,7 @@ namespace Baze_de_Date
             txt.ReadOnly = true;
             txt.TabStop = false;
             p.Controls.Add(txt) ;
-            Label com = new Label();
+            com = new Label();
             com.Name = "lbl2";
             com.Text = "Comenteaza:";
             com.Top = top + 25;
@@ -94,7 +143,7 @@ namespace Baze_de_Date
             com.Font =new System.Drawing.Font( "Arial", 9,FontStyle.Bold);
             com.ForeColor = System.Drawing.Color.Blue;
             p.Controls.Add(com);
-            Label like = new Label();
+            like = new Label();
             like.Name = "lbl2";
             like.Text = "Imi place:";
             like.Top = top + 25;
